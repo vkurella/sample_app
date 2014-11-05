b8c8a742708bf025b29567aa6766e2ef9f5eb921
VK12 <vikramkurella@gmail.com>
2014-11-05 11:19:07 -0600

    Finish Static Pages Exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
+  describe "Contact page" do
+it "should have the h1 'Contact Us'" do
+visit '/static_pages/contact'
+
+page.should have_selector('h1', :text => 'Contact Us')
+end
+it "should have the title 'Contact Us'" do
+visit '/static_pages/contact'
+page.should have_selector('title', :text => "#{base_title} | Contact Us")
+end
+end
   
   describe "Home page" do
 it "should have the h1 'Sample App'" do
@@ -9,7 +22,7 @@ page.should have_selector('h1', :text => 'Sample App')
 end
 it "should have the title 'Home'" do
 visit '/static_pages/home'
-page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+page.should have_selector('title', :text => "#{base_title} | Home")
 end
 end
 
@@ -20,7 +33,7 @@ page.should have_selector('h1', :text => 'Help')
 end
 it "should have the title 'Help'" do
 visit '/static_pages/help'
-page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+page.should have_selector('title', :text => "#{base_title} | Help")
 end
 end
 
@@ -32,7 +45,7 @@ page.should have_selector('h1', :text => 'About Us')
 end
 it "should have the title 'About Us'" do
 visit '/static_pages/about'
-page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+page.should have_selector('title', :text => "#{base_title} | About Us")
 end
 end
 end
